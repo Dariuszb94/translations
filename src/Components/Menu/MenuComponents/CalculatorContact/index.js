fe534fe4bededf436cf749a4e7cd02837f3cb9f6
dariuszb <dariusz.berer@gmail.com>
2020-11-22 13:23:49 +0100

    logo change

---
+++ src/Components/Menu/MenuComponents/CalculatorContact/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CalculatorContact = () => {
+  return (
+    <ul className="calculatorContact">
+      <li>
+        <a className="calculatorContact__contact">Kontakt</a>
+      </li>
+      <li>
+        <a className="calculatorContact__calculator">Kalkulator</a>
+      </li>
+    </ul>
+  );
+};
+
+export default CalculatorContact;
--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Logo from "./MenuComponents/Logo/index";
 import Navi from "./MenuComponents/Navi/index";
+import CalculatorContact from "./MenuComponents/CalculatorContact/index";
 
 const Menu = () => {
   return (
     <section className="menu">
       <Logo />
       <Navi />
+      <CalculatorContact />
     </section>
   );
 };
