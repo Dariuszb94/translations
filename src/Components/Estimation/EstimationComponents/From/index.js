91cc73fc8d3a6f7363beb868b7cd536568998663
dariuszb <dariusz.berer@gmail.com>
2020-11-27 22:18:23 +0100

    estimation first dropdown

--- src/Components/Estimation/EstimationComponents/From/index.js
+++ src/Components/Estimation/EstimationComponents/From/index.js
@@ -2,13 +2,83 @@ import React, { useState } from "react";
 
 const From = () => {
   const [listOpacity, showList] = useState(0);
+  const [language, changeLanguage] = useState("Amharski");
   return (
     <div className="from">
       <div onClick={() => showList(!listOpacity)}>Polski</div>
       <ul className={`from__list${listOpacity ? "--shown" : "--hidden"}`}>
-        <li>x</li>
-        <li>x</li>
-        <li>x</li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Albański")}
+        >
+          Albański{language}
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Amharski")}
+        >
+          Amharski
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Amharski")}
+        >
+          Amharski
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Albański")}
+        >
+          Albański
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Amharski")}
+        >
+          Amharski
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Amharski")}
+        >
+          Amharski
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Albański")}
+        >
+          Albański
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Amharski")}
+        >
+          Amharski
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Amharski")}
+        >
+          Amharski
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Albański")}
+        >
+          Albański
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Amharski")}
+        >
+          Amharski
+        </li>
+        <li
+          className="from__list__element"
+          onClick={() => changeLanguage("Amharski")}
+        >
+          Amharski
+        </li>
       </ul>
     </div>
   );
