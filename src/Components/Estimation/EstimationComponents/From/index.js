465aa727c19646e6863cbf948ddaa229b0344305
dariuszb <dariusz.berer@gmail.com>
2020-11-29 16:09:09 +0100

    dropdown estimate

--- src/Components/Estimation/EstimationComponents/From/index.js
+++ src/Components/Estimation/EstimationComponents/From/index.js
@@ -1,84 +1,46 @@
 import React, { useState } from "react";
+import CheckIcon from "@material-ui/icons/Check";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const From = () => {
   const [listOpacity, showList] = useState(0);
   const [language, changeLanguage] = useState("Amharski");
+  const languages = [
+    "Albański",
+    "Amharski",
+    "Angielski (amerykański)",
+    "Angielski (brytyjski)",
+    "Arabski",
+    "Armeński",
+    "Azerski",
+    "Bahamski kreolski angielski",
+    "Bajan",
+    "Baskijski (euskara)",
+    "Bemba",
+  ];
   return (
     <div className="from">
-      <div onClick={() => showList(!listOpacity)}>Polski</div>
+      <div
+        onClick={() => showList(!listOpacity)}
+        className={`from__language${listOpacity ? "--active" : ""}`}
+      >
+        <div>{language}</div> <ExpandMoreIcon />
+      </div>
       <ul className={`from__list${listOpacity ? "--shown" : "--hidden"}`}>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Albański")}
-        >
-          Albański{language}
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Amharski")}
-        >
-          Amharski
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Amharski")}
-        >
-          Amharski
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Albański")}
-        >
-          Albański
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Amharski")}
-        >
-          Amharski
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Amharski")}
-        >
-          Amharski
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Albański")}
-        >
-          Albański
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Amharski")}
-        >
-          Amharski
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Amharski")}
-        >
-          Amharski
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Albański")}
-        >
-          Albański
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Amharski")}
-        >
-          Amharski
-        </li>
-        <li
-          className="from__list__element"
-          onClick={() => changeLanguage("Amharski")}
-        >
-          Amharski
-        </li>
+        {languages.map((item, index) => (
+          <li
+            key={index}
+            className={`from__list__element${
+              language === item ? "--active" : ""
+            }`}
+            onClick={() => {
+              changeLanguage(item);
+              showList(0);
+            }}
+          >
+            {item} {language === item ? <CheckIcon /> : null}
+          </li>
+        ))}
       </ul>
     </div>
   );
