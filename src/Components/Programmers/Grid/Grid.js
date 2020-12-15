5073da687fc647d673ebe31d21e10eb16c2871c4
dariuszb <dariusz.berer@gmail.com>
2020-12-15 14:13:29 +0100

    programmers grid

--- src/Components/Programmers/Grid/Grid.js
+++ src/Components/Programmers/Grid/Grid.js
@@ -1,40 +1,25 @@
 import React from "react";
-import Overall from "./GridComponents/Overall";
 import Wp from "../../../Assets/wordpress.png";
+import Hugo from "../../../Assets/hugo.png";
+import Joomla from "../../../Assets/joomla.png";
+import Magento from "../../../Assets/magento.png";
+import Python from "../../../Assets/python.png";
+import Sitecore from "../../../Assets/sitecore.png";
+import Android from "../../../Assets/android.png";
+import Github from "../../../Assets/github.png";
+
 const Culture = () => {
+  const list = [Wp, Hugo, Joomla, Magento, Python, Sitecore, Android, Github];
   return (
     <section className="grid">
       <ul className="grid-inner">
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
-        <li className="grid__element">
-          <img src={Wp} />
-        </li>
+        {list.map((element, i) => {
+          return (
+            <li key={i} className="grid__element">
+              <img className="grid__element__img" src={element} />
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
--- src/Components/Programmers/Grid/GridComponents/Overall.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Overall = () => {
-  return <div className="grid-inner"></div>;
-};
-
-export default Overall;
